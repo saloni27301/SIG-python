341ebfbc0d31359d418c977780cd79abeab4c8fc
saloni27301 <saloni27301@gmail.com>
2021-06-13 16:54:53 +0530

    Module 7 programs

---
+++ module7/checkrange.py
@@ -0,0 +1,7 @@
+print("1803010120 Saloni")
+def test_range(n):
+    if n in range(3,9):
+        print(" %s is in the range"%str(n))
+    else:
+        print("The number is outside the given range.")
+test_range(7)
---
+++ module7/multiplylist.py
@@ -0,0 +1,10 @@
+print("1803010120 Saloni")
+def multiplyList(myList):
+    result=1
+    for x in myList:
+        result=result * x
+    return result
+list1=[1,2,3]
+list2=[3,2,4]
+print(multiplyList(list1))
+print(multiplyList(list2))
